e46a44c908210db7a19991792ddec582fe02ca54
ZSP <zhaoshipan@gmail.com>
2022-03-25 11:32:01 +0800

    save

--- CSharpDemo/Program.cs
+++ CSharpDemo/Program.cs
@@ -1,16 +1,70 @@
-using CSharpDemo.BaseApi.Enums;
-using CSharpDemo.BaseApi.Linq;
-using CSharpDemo.BaseApi.Strings;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSharpDemo
 {
-    public static class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            StringExample.TestString();
-            MyEnumTest.Test();
-            LinqExample.TestLinq();
+            // StringExample.TestString();
+            // MyEnumTest.Test();
+            // LinqExample.TestLinq();
+
+            var program = new Program();
+
+
+            Console.WriteLine($"test {Thread.CurrentThread.ManagedThreadId}");
+
+            // await 
+            await program.PrintAsync();
+            Console.WriteLine($"after PrintAsync {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000);
+        }
+
+        public async Task PrintAsync()
+        {
+            Console.WriteLine($"PrintAsync {Thread.CurrentThread.ManagedThreadId}");
+
+            var sumTask = SumTask();
+            var sumTaskResult =  sumTask.Result;
+            Console.WriteLine($" after SumTask {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public async Task<int> SumTask()
+        {
+            Console.WriteLine($"build SumTask {Thread.CurrentThread.ManagedThreadId}");
+
+            // var run = Task.Run(async () =>
+            // {
+            //     Console.WriteLine($"SumTaskResult invoking by {Thread.CurrentThread.ManagedThreadId}");
+            //     int sum = 0;
+            //     for (int i = 0; i < 100000; i++)
+            //     {
+            //         sum = +i;
+            //     }
+            //
+            //     await File.WriteAllBytesAsync("e.txt", new Byte[10]);
+            //
+            //     Console.WriteLine($"SumTaskResult {sum} by {Thread.CurrentThread.ManagedThreadId}");
+            //     return sum;
+            // });
+            
+            Console.WriteLine($"SumTaskResult invoking by {Thread.CurrentThread.ManagedThreadId}");
+            int sum = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                sum = +i;
+            }
+
+            // await File.WriteAllBytesAsync("e.txt", new Byte[10]);
+
+            Console.WriteLine($"SumTaskResult {sum} by {Thread.CurrentThread.ManagedThreadId}");
+            return sum;
+
+            // return run;
         }
     }
 }
